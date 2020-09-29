f5b59947b2d2817faa83dbfdf52881964507d707
Nicholas Gibson <nckgibson4@gmail.com>
2020-09-29 10:52:07 -0700

    make it better

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://dbuser:dbpassword@cluster0.neovh.mongodb.net/mern-auth?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+}
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require('cors');
+const mongoose = require("mongoose");
+const app = express();
+app.use(cors());
+const passport = require('passport');
+const path = require('path');
+
+const users = require('./routes/api/users');
+const plaid = require('./routes/api/plaid');
+
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+app.use("/api/plaid", plaid);
+
+if (process.env.NODE_ENV === 'production')
+{
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
